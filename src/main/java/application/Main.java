0359e17d03eeb5a4eb2d9210cf891e2c8eedb79e
Olimpusyatina <olimpus@olimpus.pro>
2022-04-15 00:13:57 +0300

    ready

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -12,7 +12,7 @@ import java.io.Writer;
 public class Main {
 
     static final Gson gson = new Gson();
-    public static void main(String ... args){
+    public static void main(String[] args){
         try {
             Rectangles rects = gson.fromJson(new FileReader(args[0]), Rectangles.class);
             Rectangle intersectionRectangle = rects.getRectangles().get(0);
